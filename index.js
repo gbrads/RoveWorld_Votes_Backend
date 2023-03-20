8e7227de801838d4b5218227f980e0c013bbd370
Gbrads <84822181+gbrads@users.noreply.github.com>
2023-03-19 23:53:15 -0700

    add text input, lock, and change vote

--- index.js
+++ index.js
@@ -16,30 +16,64 @@ app.get('/votes', (req, res) => {
 });
 
 app.post('/votes', (req, res) => {
-    const data = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
-    
-    // Merge new tweetVotes with existing tweetVotes
-    const updatedTweetVotes = {
-      ...data.tweetVotes,
-      ...req.body.tweetVotes,
-    };
-  
-    // Merge new teamScores with existing teamScores
-    const updatedTeamScores = {
-      ...data.teamScores,
-      ...req.body.teamScores,
-    };
-  
-    const newData = {
-      teamScores: updatedTeamScores,
-      tweetVotes: updatedTweetVotes,
-    };
-  
-    fs.writeFileSync(dbFile, JSON.stringify(newData, null, 2));
-    res.json(newData);
-  });  
+  const data = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
 
-  const port = process.env.PORT || 7001;
-  app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
+  // Merge new tweetVotes with existing tweetVotes
+  const updatedTweetVotes = {
+    ...data.tweetVotes,
+    ...req.body.tweetVotes,
+  };
+
+  // Merge new teamScores with existing teamScores
+  const updatedTeamScores = {
+    ...data.teamScores,
+    ...req.body.teamScores,
+  };
+
+  const newData = {
+    teamScores: updatedTeamScores,
+    tweetVotes: updatedTweetVotes,
+  };
+
+  fs.writeFileSync(dbFile, JSON.stringify(newData, null, 2));
+  res.json(newData);
+});
+
+app.put('/lock/:tweetId', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
+
+  const { tweetId } = req.params;
+  const { locked } = req.body;
+
+  if (data.tweetVotes[tweetId]) {
+    data.tweetVotes[tweetId].locked = locked;
+  } else {
+    data.tweetVotes[tweetId] = { locked };
+  }
+
+  fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+
+  res.status(200).json({ message: "Locked state updated" });
+});
+
+app.put('/comment/:tweetId', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
+
+  const { tweetId } = req.params;
+  const { comment } = req.body;
+
+  if (data.tweetVotes[tweetId]) {
+    data.tweetVotes[tweetId].comment = comment;
+  } else {
+    data.tweetVotes[tweetId] = { comment };
+  }
+
+  fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+
+  res.status(200).json({ message: "Comment updated" });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
